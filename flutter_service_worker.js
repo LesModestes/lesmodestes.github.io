12730907003418b8f371b649012814941f4c34a2
Sylvain <srochet@slimpay.com>
2021-05-28 10:41:34 +0200

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,8 +59,8 @@ const RESOURCES = {
 "manifest.json": "b9f4984eadf27ad2e6ddf709e400ad9d",
 "main.dart.js": "3787e3487ce934d20534d76376f07734",
 "favicon.png": "6049b11f84a586d32c5f6fd3c17527e5",
-"index.html": "19b90858c16dc11e74ab454519e4d465",
-"/": "19b90858c16dc11e74ab454519e4d465",
+"index.html": "5b2c1229af7a5d53245536881e25b1a6",
+"/": "5b2c1229af7a5d53245536881e25b1a6",
 "robots.txt": "96108642adb6973a93e0c4af826d1d11"
 };
 
